204f5f8b9a1643fa2cc9cfd0e3f5f4b3c1bb25e0
Radhe <kumarshivpujan9087@gmail.com>
2025-12-30 23:27:31 +0000

    learned datatypes

---
+++ 01_js_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+ "use strict";  //treat all as a JS newer version
+
+ //alert(3 + 6); //we are using nodejs, not browser
+
+let name = "radhe"
+let age = 20
+let isLoggedin = false
+
+let state;
+
+
+//number => (2 to the power 53) <- range
+//bigint
+//string => "" (prefer to write in double quote)
+//boolean => true/false
+//null => standalone value
+//undefined => value not assigned to variable/const
+//symbol => to find uniquness
+
+
+//object
+
+console.log(typeof undefined);  // O/P- undefined
+console.log(typeof null); // O/P- object
+
+
