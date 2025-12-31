45a3a446be9be80b04706b3d25d0594f68db6b15
Radhe <kumarshivpujan9087@gmail.com>
2025-12-31 00:22:32 +0000

    conversion of datatypes

---
+++ 01_js_basics/03_conversionOperation.js
@@ -0,0 +1,29 @@
+let score = "33"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+// console.log(valueInNumber);
+
+
+// NOTE* 
+//"33" => 33
+//"33abc" => NaN 
+// True => 1 , false=> 0
+
+let isLoggedIn = "radhe"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => True, 0 => False
+// "" => false
+// "radhe" => True
+
+
+let someNumber = 69
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
