d11d37cc93bd4ee2a9a59622b2b2d3ecd397660c
Radhe <kumarshivpujan9087@gmail.com>
2025-12-31 21:23:49 +0000

    operations

--- 01_js_basics/03_conversionOperation.js
+++ 01_js_basics/03_conversionOperation.js
@@ -14,7 +14,7 @@ let valueInNumber = Number(score)
 
 let isLoggedIn = "radhe"
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // 1 => True, 0 => False
 // "" => false
@@ -23,7 +23,59 @@ console.log(booleanIsLoggedIn);
 
 let someNumber = 69
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// ************************************Operations**************************************************
+
+let value = 3
+let negvalue = -value
+// console.log(negvalue);
+
+let str1 = "hello"
+let str2 = " radhe"
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+
+// console.log(true);
+// console.log(+true);   O/P-> 1
+
+let num1, num2, num3
+num1=num2=num3 = 2 + 2
+
+let gameCounter = 100
+// gameCounter++;   //101
+++gameCounter;
+// console.log(gameCounter);
+
+//post increment
+let x = 25
+y = x++;
+// console.log(`x : ${x}, y:${y}`); // O/P-> x:26, y:25
+
+//preIncrement
+let p = 9
+q = ++p;
+console.log(`p: ${p}, q:${q}`); // O/P-> p:10, q:10
+ 
+
+
+
+
+
+
+
+
+
+
+
+
 
 
