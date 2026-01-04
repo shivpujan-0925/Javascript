a01869651d3be223a6549ef4df8517436dec727e
Radhe <kumarshivpujan9087@gmail.com>
2026-01-04 14:30:10 +0000

    learn Number and maths

---
+++ 01_js_basics/06_nums_and_maths.js
@@ -0,0 +1,52 @@
+const score = 400
+// console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString());
+// console.log(typeof balance.toString());
+
+// console.log(balance.toFixed(2));
+
+const otherNumber = 253.80765
+// console.log(otherNumber.toPrecision(3));
+
+const hundred = 1000000
+// console.log(hundred.toLocaleString("en-IN"));
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ Maths ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-9));
+// console.log(Math.ceil(7.3));
+// console.log(Math.round(8.58));
+// console.log(Math.floor(25.6));
+// console.log(Math.min(2,8,4,1,4));
+// console.log(Math.max(2,8,4,1,4));
+
+console.log(Math.random());
+console.log((Math.random() * 10) + 1 );
+console.log(Math.floor(Math.random() * 10) + 1 );
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
