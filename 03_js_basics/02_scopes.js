9c3b8a92908379e37160477427e815c2af519948
Radhe <kumarshivpujan9087@gmail.com>
2026-01-08 14:46:13 +0000

    scope level and mini hoisting in js

--- 03_js_basics/02_scopes.js
+++ 03_js_basics/02_scopes.js
@@ -6,14 +6,55 @@ let a = 300
 if(true){
    let a = 10
    const b = 20
-   console.log("INNER:", a);
+//    console.log("INNER:", a);
    
-  
 }
 
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
 
+function One(){
+    const username = "Radhe"
+    function two(){
+        const website = "youtube"
+        console.log(username);
+        
+    }
+    // console.log(website);
+    two();
+    
+}
+// One();
+
+if(true){
+    const username = "radhe"
+    if(username === "radhe"){
+        const website = " youtube"
+        // console.log(username + website);
+        
+    }
+    // console.log(website);
+    
+
+}
+// console.log(username);
+
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++ Interesting +++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+console.log(addOne(6));
+
+function addOne(num){
+     return num + 1
+
+}
+
+addTwo(7);
+const addTwo = function(num){
+    return num + 2
+
+}
 
