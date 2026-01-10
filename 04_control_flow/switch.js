b26e0345f4c1de6f6eca9245733e78db3a308988
Radhe <kumarshivpujan9087@gmail.com>
2026-01-10 14:50:16 +0000

    switch

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,91 @@
+//switch :
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+// const month = 3
+// switch (month) {
+//     case 1:
+//          console.log("January");
+         
+//         break;
+//     case 2:
+//          console.log("february");
+         
+//         break;
+//     case 3:
+//          console.log("march");
+         
+//         break;
+//     case 4:
+//          console.log("April");
+         
+//         break;
+//     case 5:
+//          console.log("May");
+         
+//         break;
+//     case 6:
+//          console.log("June");
+         
+//         break;
+//     case 7:
+//          console.log("July");
+         
+//         break;
+//     case 8:
+//          console.log("August");
+         
+//         break;
+//     case 9:
+//          console.log("september");
+         
+//         break;
+//     case 10:
+//          console.log("october");
+         
+//         break;
+//     case 11:
+//          console.log("November");
+         
+//         break;
+//     case 12:
+//          console.log("December");
+         
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+const month ="march"
+
+switch (month) {
+    case "jan":
+           console.log("january");
+           
+        break;
+    case "feb":
+           console.log("february");
+           
+        break;
+    case "march":
+           console.log("march");
+           
+        break;
+    case "jan":
+           console.log("april");
+           
+        break;
+
+    default:
+        break;
+}
