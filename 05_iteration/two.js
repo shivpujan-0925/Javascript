a0e44b4555c96932554b7cb0bbc32b0c9a821615
shivpujan kumar <kumarshivpujan9087@gmail.com>
2026-01-11 17:22:30 +0000

    while , do-while loop

---
+++ 05_iteration/two.js
@@ -0,0 +1,37 @@
+// While:
+
+//Syntax-
+// while (condition) {
+    
+// }
+
+let index = 0
+while (index <= 10) {
+    // console.log(`Value of index is ${index}`);
+    index = index + 2;
+    
+}
+
+let myArray = ["Batman", "stark", "superman", "jon snow"]
+let arr = 0
+while (arr < myArray.length) {
+    // console.log(`Heroes: ${myArray[arr]}`);
+    arr = arr + 1
+
+    
+}
+
+// Do-while:
+ 
+//synatx-
+// do {
+    
+// } while (condition);
+
+
+let score = 11;
+
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score <=10);
