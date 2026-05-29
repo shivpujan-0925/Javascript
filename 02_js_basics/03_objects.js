0fd087e8f2b26e02289f1d6fc3682c0741ca82c5
shivpujan <kumarshivpujan9087@gmail.com>
2026-05-29 08:16:02 +0000

    revisit objects in js

--- 02_js_basics/03_objects.js
+++ 02_js_basics/03_objects.js
@@ -6,7 +6,7 @@
 const mySym = Symbol("key1")
 
 const JsUser = {
-    "full name":"shiv pujan",
+    "full name":"shivpujan",
     [mySym]:"myKey1",
     name: "radhe",
     age: 20,
@@ -15,12 +15,14 @@ const JsUser = {
     isLoggedIn: false
 }
 
+
 // console.log(JsUser.email);
 // console.log(JsUser["email"]);
 // console.log(JsUser["full name"]);
 // console.log(JsUser[mySym]);
 
 JsUser.email = "radhe@chatgpt.com"
+// console.log(JsUser.email);
 // Object.freeze(JsUser)        //after freez the object, can't change anything in Onjects.
 JsUser.email = "radhe@gmail.com"
 // console.log(JsUser);
@@ -34,8 +36,31 @@ JsUser.greetingTwo = function(){
     
 }
 
-console.log(JsUser.greeting());
-console.log(JsUser.greetingTwo());
+// console.log(JsUser.greeting());
+// console.log(JsUser.greetingTwo());
+
+
+//____________________________________________________________________________________________________________
+
+           //revisit  objects learning
+
+
+  const computer = {
+    name : "samsung glaxybook",
+    storage : 512,
+    ram: 16,
+    speaker : "Dolby atoms",
+    owner : "radhe"
+
+}
+computer.name = "samsung glaxybook4";    
+console.log(computer.name);
+console.log(computer.owner);
+
+// Object.freeze(computer.owner);   //Not works ? why  : owner is a simple string , Not a objects
+Object.freeze(computer)    //it works and [completely locked all properties inside  computer objects]
+computer.owner = "shivu"   //Not change becoz objects are freezed
+console.log(computer)
 
 
 
