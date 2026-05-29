8109b5f75b96b26eb8e61d485cec27e8cf9075d8
shivpujan <kumarshivpujan9087@gmail.com>
2026-05-29 06:58:57 +0000

    revise array

--- 02_js_basics/02_array.js
+++ 02_js_basics/02_array.js
@@ -4,13 +4,14 @@ const dc_heros = ["superman", "batman", "flash"]
 // marvel_heros.push(dc_heros)
 
 // console.log(marvel_heros);
-// console.log(marvel_heros[3][1]);
+// console.log(marvel_heros[3][1]);    //give err
 
  const allHeros = marvel_heros.concat(dc_heros)    //concat method
 //  console.log(allHeros);
 
-const all_new_heros = [...marvel_heros, ...dc_heros]    //spread method
-// console.log(all_new_heros);
+//spread method
+const all_newHeroes = [...dc_heros, ...marvel_heros];
+// console.log(all_newHeroes);
 
 
 const another_array = [1,2,3,[4,5,6], 7, [6,7,[1,2,3]]]
@@ -18,10 +19,10 @@ const another_array = [1,2,3,[4,5,6], 7, [6,7,[1,2,3]]]
 const real_another_array = another_array.flat(Infinity)
 // console.log(real_another_array);
 
-console.log(Array.isArray("radhe"));
-console.log(Array.from("radhe"));   //to convert into array formate
+// console.log(Array.isArray("radhe"));
+// console.log(Array.from("radhe"));   //to convert into array formate     //array.from() check for .length then convert into array formate
 
-console.log(Array.from({name:"radhe"}));  //interesting case 
+console.log(Array.from({name:"radhe"}));  //interesting case    => gives O/P : [] , because plain objects dont have length.
 
 let score1 = 100
 let score2 = 200
